5b338c4d1a08e6647df22aec169731bbc032207a
DarwishKhan1 <darwishkhan772@gmail.com>
2020-07-03 00:05:47 -0700

    Updates

---
+++ precache-manifest.094d56565ab8f698ffab380bff5bee58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39fce1018d768eafda8083a2c770cb5",
+    "url": "/PUSH/index.html"
+  },
+  {
+    "revision": "54599ae0636c60b12778",
+    "url": "/PUSH/static/css/main.d96a7019.chunk.css"
+  },
+  {
+    "revision": "7ac62c33225ec7a340fb",
+    "url": "/PUSH/static/js/2.a18048fe.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/PUSH/static/js/2.a18048fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54599ae0636c60b12778",
+    "url": "/PUSH/static/js/main.e339bb73.chunk.js"
+  },
+  {
+    "revision": "3867afbe90a00672020f",
+    "url": "/PUSH/static/js/runtime-main.3aeed13b.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/PUSH/static/media/showcase.4b31330b.jpg"
+  }
+]);
